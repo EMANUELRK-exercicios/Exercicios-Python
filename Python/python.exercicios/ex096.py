ce806b24fa93f003677e7bd73ec9b24e76187ea9
EMANUELRK-exercicios <emanuelricardookichel@gmail.com>
2023-09-24 02:17:40 -0300

    Exercicios Python

---
+++ Python/python.exercicios/ex096.py
@@ -0,0 +1,11 @@
+def titulo(msg):
+    print()
+    print(msg)
+    print('-' * 22)
+titulo(' Controle de Terrenos')
+def area(a, b):
+    t = a * b
+    print(f'A área de um terreno {a} x {b} é de {t:.1f}m²')
+l = float(input('LARGURA(m): '))
+c = float(input('COMPRIMENTO(m): '))
+area(l, c)
---
+++ Python/python.exercicios/ex097.py
@@ -0,0 +1,8 @@
+def mensagem(msg):
+    tot = len(msg) + 2
+    print('~' * tot)
+    print(msg)
+    print('~' * tot)
+mensagem('  Gustavo Guanabara')
+mensagem('  Curso de Python no Youtube')
+mensagem('  CeV')
