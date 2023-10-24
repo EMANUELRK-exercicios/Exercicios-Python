a57c7c26da1cf69ee754bda01d7730a336f621d7
EMANUELRK-exercicios <emanuelricardookichel@gmail.com>
2023-10-24 15:32:46 -0300

    Exercicios Python

--- Python/python.exercicios/ex096.py
+++ Python/python.exercicios/ex096.py
@@ -2,10 +2,17 @@ def titulo(msg):
     print()
     print(msg)
     print('-' * 22)
+
+
 titulo(' Controle de Terrenos')
+
+
 def area(a, b):
     t = a * b
     print(f'A área de um terreno {a} x {b} é de {t:.1f}m²')
+
+
+print()
 l = float(input('LARGURA(m): '))
 c = float(input('COMPRIMENTO(m): '))
 area(l, c)
